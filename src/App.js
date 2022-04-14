7503047e97efd3793fe9f734fc3f21488280ff01
lover007 <ibrahimpc14@gmail.com>
2022-04-14 10:32:20 +0600

    ecomerce site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+export default function App() {
+	return (
+		<div style={{ fontFamily: "roboto" }}>
+			<nav
+				className="font-lg w-full
+ text-white flex font-bold justify-start bg-blue-700 sticky"
+			>
+				<h3 className="m-1 p-2">Ecomerce</h3>
+				<h3 className="m-1 p-2">Login</h3>
+				<h4 className="m-1 p-2">Register</h4>
+			</nav>
+			<div className="bg-gray-300">
+				<div className="w-full h-screen flex items-center justify-center">
+					<form className="w-full md:w-1/3 bg-white rounded-lg">
+						<div className="flex font-bold justify-center mt-6">
+							<img
+								className="h-20 w-20"
+								src="https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_960_720.png"
+							/>
+						</div>
+						<h2 className="text-3xl text-center text-gray-700 mb-4">
+							Login Form
+						</h2>
+						<div className="px-12 pb-10">
+							<div className="w-full mb-2">
+								<div className="flex items-center">
+									<input
+										type="text"
+										placeholder="Username"
+										className="-mx-6 px-8  w-full border rounded px-3 py-2 text-gray-700 focus:outline-blue-400"
+									/>
+								</div>
+							</div>
+							<div className="w-full mb-2">
+								<div className="flex items-center">
+									<input
+										type="text"
+										placeholder="Password"
+										className="-mx-6 px-8 w-full border rounded px-3 py-2 text-gray-700 focus:outline-blue-400"
+									/>
+								</div>
+							</div>
+              <div className="flex items-center ">
+              <input type='checkbox' classsName=" m-2 p-2 float-right focus:outline-blue-400"/>
+							<a href="#" className="text-xs m-2 p-2 text-gray-500 float-right ">
+								Forgot Password?
+							</a>
+              </div>
+							<button
+								type="submit"
+								className="w-full py-2 rounded-full bg-green-600 text-gray-100  focus:outline-green-400"
+							>
+								Button
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 }
-
-export default App;
---
+++ src/Components/Register.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+export default function Register() {
+	return (
+		<div>
+			<nav className="font-lg w-full text-white flex font-bold justify-start bg-blue-700 sticky">
+				<h3 className="m-1 p-2">Ecomerce</h3>
+				<h3 className="m-1 p-2">Login</h3>
+				<h4 className="m-1 p-2">Register</h4>
+				{}
+			</nav>
+			<div className="flex items-center justify-center min-h-screen bg-gray-100">
+				<div className="px-8 py-6 mx-4 mt-4 text-left bg-white shadow-lg md:w-1/3 lg:w-1/3 sm:w-1/3">
+					<h3 className="text-2xl font-bold text-center">Join us</h3>
+					<form action="">
+						<div className="mt-4">
+							<div>
+								<label className="block" for="Name">
+									Name
+								</label>
+								<input
+									type="text"
+									placeholder="Name"
+									className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+								/>
+							</div>
+							<div className="mt-4">
+								<label className="block" for="email">
+									Email
+								</label>
+								<input
+									type="text"
+									placeholder="Email"
+									className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+								/>
+							</div>
+							<div className="mt-4">
+								<label className="block">Password</label>
+								<input
+									type="password"
+									placeholder="Password"
+									className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+								/>
+							</div>
+							<div className="mt-4">
+								<label className="block">Confirm Password</label>
+								<input
+									type="password"
+									placeholder="Password"
+									className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
+								/>
+							</div>
+							<span className="text-xs text-red-400">
+								Password must be same!
+							</span>
+							<div className="flex">
+								<button className="w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">
+									Create Account
+								</button>
+							</div>
+							<div className="mt-6 text-grey-dark">
+								Already have an account?
+								<a className="text-blue-600 hover:underline" href="#">
+									Log in
+								</a>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
